3ce27163eeaa629d36f707057f1fdd22577879b9
Rajani Lekkala <rajani.lekkala@gmail.com>
2019-10-03 23:53:56 +0200

    updated files

--- ExtraAssignment3/House.java
+++ ExtraAssignment3/House.java
@@ -1,18 +1,47 @@
 package ExtraAssignment3;
 
 public class House {
+    double area ;
+    int noOfFloors ;
+    double valueInCr ;
+    int yrOfConstruction;
+    String address ;
+    public House(double area1,int nFloors,double value,int yr,String addr){
+        this.area =area1;
+        this.noOfFloors = nFloors;
+        this.valueInCr =value;
+        this.yrOfConstruction  = yr;
+        this.address= addr;
+    }
+
     public static void main(String[] args){
-        double area=3.124 ;
-        int noOfFloors = 4;
-        String value = "1_crore";
-        int yrOfConstruction = 1890;
-        String address = "abc123";
-        System.out.println("Area of the house is: " + area);
+
+        House objHouse = new House(1.314,4,2.5,1988,"Lansmansgården");
+        System.out.println(objHouse.area);
+        System.out.println(objHouse.noOfFloors);
+        System.out.println(objHouse.valueInCr);
+        System.out.println(objHouse.address);
+        System.out.println(objHouse.yrOfConstruction);
+
+        /*System.out.println("Area of the house is: " + area);
         System.out.println("Number of floors: " + noOfFloors);
-        System.out.println("Value of the house is: " + value);
+        System.out.println("Value of the house is: " + valueInCr);
         System.out.println("Year of construction of the house is: " + yrOfConstruction);
         System.out.println("Address of the house is: " + address);
+*/
+        Person p = new Person();
+        p.setName("Rajani");
+        p.setEmployement("Un-emp");
+        p.setGender("Female");
+        p.setAge(32);
+        p.setHeightInCM(5.7);
+        System.out.println(p.getName().toString());
+        System.out.println(p.getEmployement().toString());
+        System.out.println(p.getGender().toString());
+        System.out.println(p.getAge());
+        System.out.println(p.getHeightInCM());
 
     }
 
 }
+
--- ExtraAssignment3/Person.java
+++ ExtraAssignment3/Person.java
@@ -1,4 +1,50 @@
 package ExtraAssignment3;
 
+import com.sun.jmx.snmp.SnmpUnknownAccContrModelException;
+
 public class Person {
+    private String name;
+    private String employement;
+    private String gender;
+    private int age;
+    private double heightInCM;
+
+    // Getter
+    public String getName() {
+        return name;
+    }
+
+    // Setter
+    public void setName(String newName) {
+        this.name = newName;
+    }
+
+    public String getEmployement(){
+        return employement;
+    }
+
+    public void setEmployement(String newEmp){
+        this.employement = newEmp;
+    }
+
+    public String getGender(){
+        return gender;
+    }
+
+    public void setGender(String newGender){
+        this.gender= newGender;
+    }
+    public int getAge(){
+        return age;
+    }
+    public void setAge(int newAge){
+        this.age=newAge;
+    }
+    public double getHeightInCM(){
+        return heightInCM;
+    }
+    public void setHeightInCM(double htInCM){
+        this.heightInCM = htInCM;
+    }
+
 }
